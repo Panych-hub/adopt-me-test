55e619afc3f06b0050cdcb89cdfecd2c4933bff6
Pavel Zamyatin <pavel.zamyatin@softcom.su>
2025-04-15 12:40:45 +0300

    feat: add author

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import BaseHeader from '@/components/BaseHeader.vue'
+import { MadeBy } from '@/components'
 </script>
 
 <template>
   <BaseHeader />
   <RouterView />
+  <MadeBy />
 </template>
 
 <style scoped></style>
---
+++ src/components/MadeBy.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="made-by">
+    Замятин Павел / git: Panych-hub / tg: @nepavel6
+  </div>
+</template>
+
+<style scoped>
+.made-by {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: var(--color-text);
+}
+</style>
